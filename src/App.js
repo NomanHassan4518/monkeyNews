45fd698b64f541601131bd7c98619dcf9063b8ad
Hassan <malikhassanhu55@gmail.com>
2023-09-16 20:33:48 +0500

    Barnch

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div className=''>
-  <BrowserRouter>
+  <BrowserRouter basename="/monkeyNews">
   <Navbar/>
   <Routes>
     <Route path="/" element={<Home/>}></Route>
